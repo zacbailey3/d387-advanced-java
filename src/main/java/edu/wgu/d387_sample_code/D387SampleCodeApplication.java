d25e55eedf279e8330dbe0e77927403a605ca078
zacbailey <zbail25@wgu.ed>
2025-12-18 15:00:27 -0800

    B3: Created TimeZoneUtil.java to convert time zones. Edited d387application to show timezones and message with live presentation.

--- src/main/java/edu/wgu/d387_sample_code/D387SampleCodeApplication.java
+++ src/main/java/edu/wgu/d387_sample_code/D387SampleCodeApplication.java
@@ -1,9 +1,13 @@
 package edu.wgu.d387_sample_code;
 
+import edu.wgu.d387_sample_code.util.TimeZoneUtil;
 import edu.wgu.d387_sample_code.util.WelcomeTask;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Locale;
 
 @SpringBootApplication
@@ -21,5 +25,32 @@ public class D387SampleCodeApplication {
 
 		englishThread.start();
 		frenchThread.start();
+
+		LocalDateTime presentationTime =
+				LocalDateTime.of(2025, 1, 15, 13, 0); // 1:00 PM ET
+
+		ZonedDateTime et =
+				TimeZoneUtil.convertTime(
+						presentationTime,
+						ZoneId.of("America/New_York"),
+						ZoneId.of("America/New_York"));
+
+		ZonedDateTime mt =
+				TimeZoneUtil.convertTime(
+						presentationTime,
+						ZoneId.of("America/New_York"),
+						ZoneId.of("America/Denver"));
+
+		ZonedDateTime utc =
+				TimeZoneUtil.convertTime(
+						presentationTime,
+						ZoneId.of("America/New_York"),
+						ZoneId.of("UTC"));
+
+		System.out.println("Online Live Presentation at the Landon Hotel - Presentation Times:");
+		System.out.println("ET: " + et.toLocalTime());
+		System.out.println("MT: " + mt.toLocalTime());
+		System.out.println("UTC: " + utc.toLocalTime());
 	}
+
 }
--- src/main/java/edu/wgu/d387_sample_code/util/TimeZoneUtil.java
+++ src/main/java/edu/wgu/d387_sample_code/util/TimeZoneUtil.java
@@ -1,4 +1,15 @@
 package edu.wgu.d387_sample_code.util;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 public class TimeZoneUtil {
+    public static ZonedDateTime convertTime(
+            LocalDateTime time,
+            ZoneId fromZone,
+            ZoneId toZone) {
+
+        return time.atZone(fromZone).withZoneSameInstant(toZone);
+    }
 }
